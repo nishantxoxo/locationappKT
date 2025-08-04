85f19154f9ff46123a2390bceea56f595901fffc
nishant <nishantpriya.scorpion@gmail.com>
2025-08-04 20:39:57 +0530

    m

--- app/src/main/java/com/example/locationapp/MainActivity.kt
+++ app/src/main/java/com/example/locationapp/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.locationapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.locationapp.ui.theme.LocationAppTheme
@@ -30,18 +36,27 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LocationAppTheme {
-        Greeting("Android")
+fun locationDisplay(
+    locationUtils: locationUtils,
+    context: Context
+){
+    val requestpermissionlauncher = rememberLauncherForActivityResult() { }
+
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        Text(text = "location not available")
+        Button(onClick = {
+            if(locationUtils.hasLocationPermission(context)){
+
+            }
+            else{
+
+            }
+        }) {
+            Text("get location")
+        }
+
     }
+
 }
