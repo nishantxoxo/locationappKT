2f54127361a3937faa1f559c7081b35ee3231322
nishant <nishantpriya.scorpion@gmail.com>
2025-08-05 21:11:31 +0530

    perm

--- app/src/main/java/com/example/locationapp/MainActivity.kt
+++ app/src/main/java/com/example/locationapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.locationapp
 
 import android.content.Context
 import android.os.Bundle
+import android.Manifest
+import android.widget.Toast
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,7 +21,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.example.locationapp.ui.theme.LocationAppTheme
+import kotlin.contracts.contract
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,7 +48,34 @@ fun locationDisplay(
     locationUtils: locationUtils,
     context: Context
 ){
-    val requestpermissionlauncher = rememberLauncherForActivityResult() { }
+    val requestpermissionlauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult ={
+            permissions ->
+            if(permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true &&
+                permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true
+                ){
+                //we have access
+            }
+            else{
+                //ask for permsission
+                val rationaleRequired = ActivityCompat.shouldShowRequestPermissionRationale(context as MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                    ) || ActivityCompat.shouldShowRequestPermissionRationale(context as MainActivity,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+
+
+                if(rationaleRequired){
+                    Toast.makeText(context, "Location Permission is required for this feature", Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Toast.makeText(context, "Location Permission is required for this feature", Toast.LENGTH_LONG).show()
+                    
+                }
+            }
+        }
+        )
 
     Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
         Text(text = "location not available")
